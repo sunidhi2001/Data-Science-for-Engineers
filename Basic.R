f308dd0acd5bb62befb6f7521310ea8a79778e8a
SUNIDHI PANDEY <65342463+sunidhi2001@users.noreply.github.com>
2020-09-27 10:24:15 +0530

    Adding Basics of R

---
+++ Basic.R
@@ -0,0 +1,21 @@
+#Adding a Comment 
+#This program calculate a and b
+
+a = 11
+b = a*10
+print(c(a,b))
+
+#Find Data type of object : use command typeof()
+typeof(1)
+typeof("22-01-2001")
+
+#Verify if object is a of certain data type
+#Use prefix "is." before data type as command.
+is.character("21-01-2001")
+is.character(as.Date("20-01-2001"))
+
+#Coerce or convert data type of object to another
+#Use prefix "as." before datatype  as command.
+as.complex(2)
+as.numeric(a)
+as.numeric("a")
